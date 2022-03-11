9abee669d41c7f4c1d6d8759b2bac1e1a53d2431
nripayena <67066743+nripayena@users.noreply.github.com>
2022-03-11 20:00:06 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,17 +1,19 @@
+var name1=prompt("Player 1's name?");
+var name2=prompt("Player 2's name?");
 var n1 = Math.floor( Math.random() * 6) + 1;
 var n2 = Math.floor( Math.random() * 6) + 1;
 var dice_img1 = "dice" + n1 + ".png";
 var dice_img2 = "dice" + n2 + ".png";
 if(n1>n2)
 {
-    document.querySelector("h1").textContent="🚩Player1 Wins!";
+    document.querySelector("h1").textContent="🚩"+name1+" Wins!";
 }
 else if(n1===n2)
 {
     document.querySelector("h1").textContent="Draw";   
 }
 else{
-    document.querySelector("h1").textContent="🚩Player2 Wins!";
+    document.querySelector("h1").textContent="🚩"+name2+" Wins!";
 }
 
 document.querySelectorAll("img")[0].setAttribute("src",dice_img1);
